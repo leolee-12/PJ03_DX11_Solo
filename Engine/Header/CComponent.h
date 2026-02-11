64694b314212e6a952d9a399b226895c9a1cab37
LEO <leo.lee.dec@gmail.com>
2026-02-11 23:10:38 +0900

    [feat] CComponent Owner Setter/Getter 추가

--- Engine/Header/CComponent.h
+++ Engine/Header/CComponent.h
@@ -4,6 +4,8 @@
 
 BEGIN(Engine)
 
+class CGameObject;
+
 class ENGINE_DLL CComponent : public CBase
 {
 protected:
@@ -13,12 +15,17 @@ protected:
 	virtual ~CComponent();
 
 public:
+	// === Setter/Getter ===
+	void Set_Owner(CGameObject* pOwner) { m_pOwner = pOwner; }
+	CGameObject* Get_Owner() const { return m_pOwner; }
+
 	virtual _int Update_Component(const _float& fTimeDelta) { return 0; }
 	virtual void LateUpdate_Component() { }
 
 protected:
 	LPDIRECT3DDEVICE9			m_pGraphicDev;
 	_bool						m_bClone;
+	CGameObject*				m_pOwner;
 
 public:
 	virtual CComponent* Clone()	PURE;
