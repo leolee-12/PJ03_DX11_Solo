705fe1fe434446528bd5e7db668c5146c2a22084
Leo <leo.lee.dec@gmail.com>
2026-02-19 22:45:38 +0900

    [feat] 2/13 수업내용 반영 중

--- jusin_DX11_Framework/DX11_Framework_LEO/Engine/Private/Graphic_Device.cpp
+++ jusin_DX11_Framework/DX11_Framework_LEO/Engine/Private/Graphic_Device.cpp
@@ -8,6 +8,36 @@ CGraphic_Device::CGraphic_Device()
 
 HRESULT CGraphic_Device::Initialize(HWND hWnd, WINMODE isWindowed, _uint iWinSizeX, _uint iWinSizeY, ID3D11Device** ppDevice, ID3D11DeviceContext** ppContext)
 {
+	_uint iFlag = 0;
+
+#ifdef _DEBUG
+	iFlag = D3D11_CREATE_DEVICE_DEBUG;
+#endif	// _DEBUG
+
+	D3D_FEATURE_LEVEL FeatureLV;
+	// 지금 생성을 수행한 그래픽카드는 DX 몇 번까지 지원하는가?
+	// -> DX11을 지원하지 않는 그래픽카드는 거의 없으므로 활용되지 않는 변수
+
+	/* 그래픽 장치 초기화 */
+	if (FAILED(D3D11CreateDevice(	nullptr, D3D_DRIVER_TYPE_HARDWARE, 0, iFlag, nullptr, 0,
+									D3D11_SDK_VERSION, &m_pDevice, &FeatureLV, &m_pContext)))
+		return E_FAIL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	return E_NOTIMPL;
 }
 
