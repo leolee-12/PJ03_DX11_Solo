279b5a912829d6932cce55f90fb6c4435c8b0b1f
Leo <leo.lee.dec@gmail.com>
2026-02-26 20:13:59 +0900

    [feat] 02/26 수업 내용 반영 - 렌더러 (BackGround 등록 및 렌더콜) - CGameInstance::Clear_Resources (특정 레벨의 객체 정리)

--- jusin_DX11_Framework/DX11_Framework_LEO/Engine/Private/GameObject.cpp
+++ jusin_DX11_Framework/DX11_Framework_LEO/Engine/Private/GameObject.cpp
@@ -1,9 +1,12 @@
 #include "GameObject.h"
+#include "GameInstance.h"
 
 CGameObject::CGameObject(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
 	: m_pDevice{ pDevice }
 	, m_pContext{ pContext }
+	, m_pGameInstance{ CGameInstance::GetInstance() }
 {
+	Safe_AddRef(m_pGameInstance);
 	Safe_AddRef(m_pDevice);
 	Safe_AddRef(m_pContext);
 }
@@ -11,7 +14,9 @@ CGameObject::CGameObject(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
 CGameObject::CGameObject(const CGameObject& Prototype)
 	: m_pDevice{ Prototype.m_pDevice }
 	, m_pContext{ Prototype.m_pContext }
+	, m_pGameInstance{ CGameInstance::GetInstance() }
 {
+	Safe_AddRef(m_pGameInstance);
 	Safe_AddRef(m_pDevice);
 	Safe_AddRef(m_pContext);
 }
@@ -51,6 +56,7 @@ void CGameObject::Free()
 {
 	__super::Free();
 
+	Safe_Release(m_pGameInstance);
 	Safe_Release(m_pContext);
 	Safe_Release(m_pDevice);
 }
