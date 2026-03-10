895088d3db32f2edc6cbe1db34fd95706c7a91ba
Leo <leo.lee.dec@gmail.com>
2026-03-10 21:52:59 +0900

    [feat] 03/09 수업 내용 반영 - Transform 컴포넌트 기능 구현 - 함수를 초기 설정용과 게임 로직용으로 구분 - 크기 (ScaleTo, Scaling), 회전(Rotation, Turn), 이동(Go_series), etc(Getter/Setter)

--- jusin_DX11_Framework/DX11_Framework_LEO/Engine/Private/Transform.cpp
+++ jusin_DX11_Framework/DX11_Framework_LEO/Engine/Private/Transform.cpp
@@ -28,6 +28,103 @@ HRESULT CTransform::Initialize(void* pArg)
 	return S_OK;
 }
 
+void CTransform::ScaleTo(_float fScaleX, _float fScaleY, _float fScaleZ)
+{
+	Set_State(STATE::RIGHT, XMVector3Normalize(Get_State(STATE::RIGHT) * fScaleX));
+	Set_State(STATE::UP, XMVector3Normalize(Get_State(STATE::UP) * fScaleY));
+	Set_State(STATE::LOOK, XMVector3Normalize(Get_State(STATE::LOOK) * fScaleZ));
+}
+
+void CTransform::Scaling(_float fScaleX, _float fScaleY, _float fScaleZ)
+{
+	Set_State(STATE::RIGHT, Get_State(STATE::RIGHT) * fScaleX);
+	Set_State(STATE::UP, Get_State(STATE::UP) * fScaleY);
+	Set_State(STATE::LOOK, Get_State(STATE::LOOK) * fScaleZ);
+}
+
+void XM_CALLCONV CTransform::Rotation(_fvector vAxis, _float fRadian)
+{
+	_float3 vScaled = Get_Scaled();
+
+	_vector vRight	= XMVectorSet(1.f, 0.f, 0.f, 0.f) * vScaled.x;
+	_vector vUp		= XMVectorSet(0.f, 1.f, 0.f, 0.f) * vScaled.y;
+	_vector vLook	= XMVectorSet(0.f, 0.f, 1.f, 0.f) * vScaled.z;
+
+	_matrix RotationMatrix = XMMatrixRotationAxis(vAxis, fRadian);
+
+	Set_State(STATE::RIGHT, XMVector3TransformNormal(vRight, RotationMatrix));
+	Set_State(STATE::UP, XMVector3TransformNormal(vUp, RotationMatrix));
+	Set_State(STATE::LOOK, XMVector3TransformNormal(vLook, RotationMatrix));
+}
+
+void XM_CALLCONV CTransform::Turn(_fvector vAxis, _float fTimeDelta)
+{
+	_float3 vScaled = Get_Scaled();
+
+	_vector vRight	= Get_State(STATE::RIGHT);
+	_vector vUp		= Get_State(STATE::UP);
+	_vector vLook	= Get_State(STATE::LOOK);
+
+	_matrix RotationMatrix = XMMatrixRotationAxis(vAxis, m_fRotationPerSec * fTimeDelta);
+
+	Set_State(STATE::RIGHT, XMVector3TransformNormal(vRight, RotationMatrix));
+	Set_State(STATE::UP, XMVector3TransformNormal(vUp, RotationMatrix));
+	Set_State(STATE::LOOK, XMVector3TransformNormal(vLook, RotationMatrix));
+}
+
+void XM_CALLCONV CTransform::LookAt(_fvector vAt)
+{
+	_float3 vScaled = Get_Scaled();
+
+	_vector vLook	= vAt - Get_State(STATE::POSITION);
+	_vector vRight	= XMVector3Cross(XMVectorSet(0.f, 1.f, 0.f, 0.f), vLook);
+
+	if(XMVectorGetX(XMVector3Length(vRight)) < 1e-6f)
+		vRight = XMVector3Cross(XMVectorSet(0.f, 0.f, 1.f, 0.f), vLook);
+
+	_vector vUp		= XMVector3Cross(vLook, vRight);
+
+	Set_State(STATE::RIGHT, XMVector3Normalize(vRight) * vScaled.x);
+	Set_State(STATE::UP, XMVector3Normalize(vUp) * vScaled.y);
+	Set_State(STATE::LOOK, XMVector3Normalize(vLook) * vScaled.z);
+}
+
+void CTransform::Go_Straight(_float fTimeDelta)
+{
+	_vector vPosition	= Get_State(STATE::POSITION);
+	_vector vLook		= Get_State(STATE::LOOK);
+
+	vPosition += XMVector3Normalize(vLook) * m_fSpeedPerSec * fTimeDelta;
+	Set_State(STATE::POSITION, vPosition);
+}
+
+void CTransform::Go_Backward(_float fTimeDelta)
+{
+	_vector vPosition = Get_State(STATE::POSITION);
+	_vector vLook = Get_State(STATE::LOOK);
+
+	vPosition -= XMVector3Normalize(vLook) * m_fSpeedPerSec * fTimeDelta;
+	Set_State(STATE::POSITION, vPosition);
+}
+
+void CTransform::Go_Left(_float fTimeDelta)
+{
+	_vector vPosition = Get_State(STATE::POSITION);
+	_vector vRight = Get_State(STATE::RIGHT);
+
+	vPosition -= XMVector3Normalize(vRight) * m_fSpeedPerSec * fTimeDelta;
+	Set_State(STATE::POSITION, vPosition);
+}
+
+void CTransform::Go_Right(_float fTimeDelta)
+{
+	_vector vPosition = Get_State(STATE::POSITION);
+	_vector vRight = Get_State(STATE::RIGHT);
+
+	vPosition += XMVector3Normalize(vRight) * m_fSpeedPerSec * fTimeDelta;
+	Set_State(STATE::POSITION, vPosition);
+}
+
 CTransform* CTransform::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
 {
 	CTransform* pInstance = new CTransform(pDevice, pContext);
--- jusin_DX11_Framework/DX11_Framework_LEO/EngineSDK/Inc/Transform.h
+++ jusin_DX11_Framework/DX11_Framework_LEO/EngineSDK/Inc/Transform.h
@@ -25,8 +25,41 @@ protected:
 	virtual ~CTransform() = default;
 
 public:
+	_vector Get_State(STATE eState) const
+	{
+		return XMLoadFloat4(reinterpret_cast
+			<const _float4*>(&m_WorldMatrix.m[ETOUI(eState)]));
+	}
+
+	void XM_CALLCONV Set_State(STATE eState, _fvector vState)
+	{
+		XMStoreFloat4(reinterpret_cast<_float4*>
+			(&m_WorldMatrix.m[ETOUI(eState)]), vState);
+	}
+	
+	_float3 Get_Scaled() const
+	{
+		return _float3(	XMVectorGetX(XMVector3Length(Get_State(STATE::RIGHT))),
+						XMVectorGetX(XMVector3Length(Get_State(STATE::UP))),
+						XMVectorGetX(XMVector3Length(Get_State(STATE::LOOK))));
+	}
+
+public:
 	virtual HRESULT	Initialize_Prototype();
 	virtual HRESULT	Initialize(void* pArg);
+
+public:
+	void ScaleTo(_float fScaleX = 1.f, _float fScaleY = 1.f, _float fScaleZ = 1.f);
+	void Scaling(_float fScaleX = 1.f, _float fScaleY = 1.f, _float fScaleZ = 1.f);
+
+	void XM_CALLCONV Rotation(_fvector vAxis, _float fRadian);
+	void XM_CALLCONV Turn(_fvector vAxis, _float fTimeDelta);
+	void XM_CALLCONV LookAt(_fvector vFocus);
+
+	void Go_Straight(_float fTimeDelta);
+	void Go_Backward(_float fTimeDelta);
+	void Go_Left(_float fTimeDelta);
+	void Go_Right(_float fTimeDelta);
 
 private:
 	_float4x4	m_WorldMatrix = {};
